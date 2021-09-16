e195e957b50e8637f6d98d56179525b1c90e97bc
Brian Diaz <bidiaz101@gmail.com>
2021-09-16 13:29:37 -0500

    Completed assignment

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {  useEffect } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, setToys}) {
+  useEffect(()=>{
+    fetch("http://localhost:3001/toys")
+    .then(resp => resp.json())
+    .then(data => setToys(data))
+  }, [])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE"
+    })
+    .then(setToys(toys.filter(toy => toy.id !== id)))
+  }
+
+  const toysToDisplay = toys.map(toy => {
+    return <ToyCard 
+      name={toy.name} 
+      image={toy.image} 
+      likes={toy.likes} 
+      id={toy.id} 
+      key={toy.id} 
+      handleDelete={handleDelete}
+    />
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toysToDisplay}</div>
   );
 }
 
